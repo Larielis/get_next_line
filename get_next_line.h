957eb3f0432a51736a014bba96c3b5a3f4f8db6a
Larielis <racamach@student.42madrid.com>
2024-11-09 23:40:16 +0100

    Norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,37 +6,37 @@
 /*   By: racamach <racamach@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:19:10 by racamach          #+#    #+#             */
-/*   Updated: 2024/11/04 13:19:10 by racamach         ###   ########.fr       */
+/*   Updated: 2024/11/09 23:22:30 by racamach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h> 
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 typedef struct s_list
 {
-    char            *content;
-    struct s_list   *next;
-}                   t_list;
+	char			*content;
+	struct s_list	*next;
+}					t_list;
 
-t_list *free_until_newline(t_list *buffer_list);
-char *get_next_line(int fd);
-t_list  *create_node(char *content);
-int newline(t_list *buffer_list);
-int ft_lstcreate_and_add_back(t_list **lst, char *content);
-char *get_line_from_buffer(t_list *buffer_list);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlcat(char *dest, const char *src, size_t n);
-int get_line_length(t_list *buffer_list);
-char	*ft_strdup(char *s1);
+t_list				*free_until_newline(t_list *buffer_list);
+char				*get_next_line(int fd);
+t_list				*create_node(char *content);
+int					newline(t_list *buffer_list);
+int					ft_lstcreate_and_add_back(t_list **lst, char *content);
+char				*get_line_from_buffer(t_list *buffer_list);
+char				*ft_strchr(const char *s, int c);
+size_t				ft_strlcat(char *dest, const char *src, size_t n);
+int					get_line_length(t_list *buffer_list);
+char				*ft_strdup(char *s1);
 
 #endif
